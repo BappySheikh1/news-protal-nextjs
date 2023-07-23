814d05bf365913b0dfbd5b76d682f74a1ad1ce54
BappySheikh1 <sheikhbappy91gmail.com>
2023-07-23 22:57:16 +0600

    getStaticProps added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
 import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
-// import Banner from "@/components/UI/Banner";
 import AllNews from "@/components/UI/AllNews";
-import { useGetNewsesQuery } from "@/redux/api/api";
 import dynamic from "next/dynamic";
 
 const HomePage = ({ allNews }) => {
-  // console.log(allNews);
-  const { data, isLoading, isError, error } = useGetNewsesQuery(); //-> redux store data
-  // console.log(data);
-
+  console.log(allNews);
 
   const DynamicBanner = dynamic(() => import("@/components/UI/Banner"), {
     loading: () => <h1>Loading...</h1>,
@@ -39,28 +34,13 @@ HomePage.getLayout = function getLayout(page) {
 };
 
 export const getStaticProps = async () => {
-  // const res = await fetch("http://localhost:3000/api/news"); // internal API connected with mongoDB
-  const res = await fetch("http://localhost:5000/news"); // --> json server
-  const data = await res.json();
-  // console.log(data);
-  return {
-    props: {
-      allNews: data,
-      // allNews: data.data, // when using internal API connected with mongoDB
-    },
-    revalidate: 10,
-  };
-};
-
-
-// for server side rendering (SSR) with json-server data
-/* export const getServerSideProps = async () => {
   const res = await fetch("http://localhost:5000/news");
   const data = await res.json();
-  // console.log(data);
+
   return {
     props: {
       allNews: data,
-    }
+    },
+    // revalidate: 10,
   };
-}; */
+};
--- src/pages/news/[newsId].js
+++ src/pages/news/[newsId].js
@@ -1,72 +1,73 @@
+import RootLayout from "@/components/Layouts/RootLayout";
 import { Col, Row } from "antd";
+import Image from "next/image";
 import {
-  UserOutlined,
+  ArrowRightOutlined,
   CalendarOutlined,
   CommentOutlined,
   ProfileOutlined,
 } from "@ant-design/icons";
-import Image from "next/image";
-import RootLayout from "@/components/Layouts/RootLayout";
 
-const NewsDetailPage = ({ news }) => (
-  <Row style={{ marginTop: "80px", alignItems: "center" }}>
+const NewsDetailPage = ({ news }) => {
+  // console.log(news);
+  return (
+    <>
+       <Row style={{ marginTop: "80px", alignItems: "center" }}>
     <Col md={6} lg={12}>
-      <Image
-        alt="example"
-        src={news?.image_url}
-        width={500}
-        height={300}
-        responsive
-      />
-    </Col>
-    <Col md={6} lg={12} style={{ paddingLeft: "20px" }}>
-      <h1 style={{ fontSize: "30px" }}>{news?.title}</h1>
-      <span
-        style={{
-          color: "gray",
-          display: "block",
-          fontSize: "20px",
-        }}
-      >
-        <UserOutlined /> {news?.author}
-      </span>
-      <div
-        className="line"
-        style={{
-          height: "5px",
-          margin: "20px 0",
-          background: "#000",
-          width: "100%",
-        }}
-      ></div>
+          <div>
+            <Image
+              src={news?.image_url}
+              width={500}
+              height={300}
+              responsive
+              alt="news image"
+            />
+          </div>
+        </Col>
+        <Col md={6} lg={12} style={{ paddingLeft: "20px" }}>
+          <div>
+          <h1 style={{ fontSize: "30px" }}>{news?.title}</h1>
+            <div
+              className="line"
+              style={{
+                height: "5px",
+                margin: "20px 0",
+                background: "#000",
+                width: "100%",
+              }}
+            ></div>
 
-      <p
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "100%",
-          color: "gray",
-          margin: "10px 0px",
-          fontSize: "20px",
-        }}
-      >
-        <span>
-          <CalendarOutlined /> {news?.release_date}
-        </span>
-        <span>
-          <CommentOutlined /> {news?.comment_count} Comments
-        </span>
-        <span>
-          <ProfileOutlined /> {news?.category}
-        </span>
-      </p>
+            <p
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+                color: "gray",
+                margin: "10px 0px",
+                fontSize: "12px",
+              }}
+            >
+              <span>
+                <CalendarOutlined /> {news?.release_date}
+              </span>
+              <span>
+                <CommentOutlined /> {news?.comment_count} COMMENTS
+              </span>
+              <span>
+                <ProfileOutlined /> {news?.category}
+              </span>
+            </p>
 
-      <p style={{ fontSize: "25px", fontWeight: "lighter" }}>
-        {news?.description}
-      </p>
-    </Col>
-  </Row>
-);
+            <p style={{ fontSize: "20px" }}>
+              {news?.description}
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
 export default NewsDetailPage;
 
 NewsDetailPage.getLayout = function getLayout(page) {
@@ -74,22 +75,20 @@ NewsDetailPage.getLayout = function getLayout(page) {
 };
 
 // export const getStaticPaths = async () => {
-//   const res = await fetch("http://localhost:5000/news");
+//   const res = await fetch(`http://localhost:5000/news`);
 //   const newses = await res.json();
-
 //   const paths = newses.map((news) => ({
 //     params: { newsId: news.id },
 //   }));
-
-//   return { paths, fallback: false };
+//   return {
+//     paths,
+//     fallback: false,
+//   };
 // };
 
-export const getServerSideProps = async (context) => {
-  const { params } = context;
+export const getServerSideProps = async ({ params }) => {
   const res = await fetch(`http://localhost:5000/news/${params.newsId}`);
   const data = await res.json();
-  // console.log(data);
-
   return {
     props: {
       news: data,
